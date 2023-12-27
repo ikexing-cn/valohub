9c1ecb51710582f2dd041363039fbc34f2cd21d4
ikexing-cn <ikexing@qq.com>
2023-12-27 18:32:30 +0800

    feat: basic prisma

---
+++ packages/server/utils/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client'
+
+let _prisma: PrismaClient
+export function usePrisma() {
+  if (!_prisma) {
+    _prisma = new PrismaClient()
+  }
+  return _prisma
+}
