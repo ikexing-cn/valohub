7fb10c38ed28a57fd6d76cbb816a866a7ee82cf0
ikexing-cn <ikexing@qq.com>
2024-01-13 14:19:22 +0800

    feat: add help for command

--- packages/bot/src/commands/index.ts
+++ packages/bot/src/commands/index.ts
@@ -1,12 +1,37 @@
 export const commands = {
   pravite: {
-    bind: ['bind', '绑定'],
-    unbind: ['unbind', '解绑', '取消绑定'],
+    bind: {
+      aliases: ['bind', '绑定'],
+      desc: '绑定 riot 账号 「仅限私聊」',
+      usage: '绑定 <别名 = default>',
+    },
+    unbind: {
+      aliases: ['unbind', '解绑'],
+      desc: '解绑 riot 账号 「仅限私聊」',
+      usage: '解绑 <别名 = default>',
+    },
+    verify: {
+      aliases: ['verify', '验证'],
+      desc: '账户所有权验证 「仅限私聊」',
+      usage: '验证 <密码> <别名 = default>',
+    },
   },
   common: {
-    ping: ['ping'],
-    help: ['help', '帮助'],
-    dailystore: ['dailystore', '每日商店'],
+    ping: {
+      aliases: ['p', 'ping'],
+      desc: '测试 bot 存活',
+      usage: 'ping',
+    },
+    help: {
+      aliases: ['h', 'help', '帮助'],
+      desc: '查看命令帮助',
+      usage: '帮助 <命令名>',
+    },
+    dailystore: {
+      aliases: ['dailystore', '每日商店'],
+      desc: '查看每日商店',
+      usage: '每日商店 <别名 = default>',
+    },
   },
 }
 
@@ -34,8 +59,8 @@ export function parseCommand(command: string) {
 
 export function findCommandBase(isGroup: boolean, command: string) {
   for (const [group, config] of Object.entries(commands)) {
-    for (const [name, aliases] of Object.entries(config)) {
-      if (aliases.includes(command.toLowerCase())) {
+    for (const [name, cmdOptions] of Object.entries(config)) {
+      if (cmdOptions.aliases.includes(command.toLowerCase())) {
         if (isGroup && group === 'pravite') {
           return null
         }
--- packages/bot/src/event.ts
+++ packages/bot/src/event.ts
@@ -1,8 +1,59 @@
 /* eslint-disable no-console */
 import { findCommandBase, parseCommand } from './commands'
 import { sendMessage } from './utils/chat'
+import { helpCommand } from './commands/help'
 import type { Tag } from 'go-cqwebsocket/out/tags'
-import type { CQWebSocket } from 'go-cqwebsocket'
+import type { CQEvent, CQWebSocket } from 'go-cqwebsocket'
+
+function handleMessage(client: CQWebSocket, event: CQEvent<'message'>) {
+  const context = event.context
+  const message = context.message
+  const isGroup = context.message_type === 'group'
+
+  if (
+    isGroup &&
+    (typeof message === 'string' ||
+      !message.some(
+        (item) => item.type === 'at' && item.data.qq === '149384916',
+      ))
+  )
+    return
+
+  const textMessage = (message as Tag[])?.find((item) => item.type === 'text')
+    ?.data?.text as string
+  if (!textMessage) return
+
+  const { command, args } = parseCommand(textMessage.trim())
+  const commandBase = findCommandBase(isGroup, command)
+
+  const send = sendMessage({
+    client,
+    isGroup,
+    messageId: context.message_id,
+    senderId: isGroup ? context.group_id : context.user_id,
+  })
+
+  switch (commandBase) {
+    case 'ping':
+      send('pong')
+      break
+    case 'help':
+      send(helpCommand(args?.[0]))
+      break
+    case 'dailystore':
+      send('todo')
+      break
+    case 'bind':
+      send('todo')
+      break
+    case 'unbind':
+      send('todo')
+      break
+    default:
+      send('未知指令')
+      break
+  }
+}
 
 export function registerEvent(client: CQWebSocket) {
   client.on('request.friend', (event) => {
@@ -15,55 +66,7 @@ export function registerEvent(client: CQWebSocket) {
     }, Math.random() * 2000)
   })
 
-  client.on('message', (event) => {
-    const context = event.context
-    const message = context.message
-    const isGroup = context.message_type === 'group'
-
-    if (
-      isGroup &&
-      (typeof message === 'string' ||
-        !message.some(
-          (item) => item.type === 'at' && item.data.qq === '149384916',
-        ))
-    )
-      return
-
-    const textMessage = (message as Tag[])?.find((item) => item.type === 'text')
-      ?.data?.text as string
-    if (!textMessage) return
-
-    const { command, args } = parseCommand(textMessage.trim())
-    const commandBase = findCommandBase(isGroup, command)
-
-    const send = sendMessage({
-      client,
-      isGroup,
-      messageId: context.message_id,
-      senderId: isGroup ? context.group_id : context.user_id,
-    })
-
-    switch (commandBase) {
-      case 'ping':
-        send('pong')
-        break
-      case 'help':
-        send('todo')
-        break
-      case 'dailystore':
-        send('todo')
-        break
-      case 'bind':
-        send('todo')
-        break
-      case 'unbind':
-        send('todo')
-        break
-      default:
-        send('未知指令')
-        break
-    }
-  })
+  client.on('message', (event) => handleMessage(client, event))
 
   client.on('socket.open', () => {
     console.log('Success opened for CQWebSocket.')
