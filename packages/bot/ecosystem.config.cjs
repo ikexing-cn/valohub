8f668ce8e41980589e4bf055754a1c478e65aca2
ikexing-cn <ikexing@qq.com>
2024-01-14 16:55:21 +0800

    feat: deploy bot [deploy]

---
+++ packages/bot/ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bot',
+      watch: false,
+      autorestart: true,
+      log: './logs/app.log',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      script: 'dist/main.mjs',
+    },
+  ],
+}
