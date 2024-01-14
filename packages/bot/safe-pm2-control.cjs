cb8bd837ef74c9f75cc6e1b96c6e029690efcce3
ikexing-cn <ikexing.cn@gmail.com>
2024-01-14 23:13:23 +0800

    feat: build cjs & remove cache [deploy]

--- packages/bot/safe-pm2-control.cjs
+++ packages/bot/safe-pm2-control.cjs
@@ -10,38 +10,45 @@ const { resolve, join } = require('node:path')
 const { manualSync } = require('rimraf')
 const { copySync, moveSync } = require('fs-extra')
 
-// try {
-//   const result = execSync('pm2 stop bot', { encoding: 'utf-8' })
-//   console.log('Stoppping bot:', result)
-// } catch {}
+const downloadUri = process.argv[2]
+const downloadDirName = process.argv[3]
+const downloadDirUri = resolve(downloadUri, downloadDirName)
+
+const realRuntimeDirName = 'deploy-bot'
+const realRuntimeDirUri = resolve(downloadUri, realRuntimeDirName)
 
 try {
   const result = execSync('pm2 delete bot', { encoding: 'utf-8' })
   console.log('Deleting bot:', result)
 } catch {}
 
-if (existsSync(resolve(process.argv[2], 'deploy-bot'))) {
-  manualSync(resolve(process.argv[2], 'deploy-bot'))
+if (existsSync(realRuntimeDirUri)) {
+  manualSync(realRuntimeDirUri)
 }
 
 try {
-  process.chdir(process.argv[2])
-  copySync('deploy-bot-build-files-download', 'deploy-bot')
+  process.chdir(downloadUri)
+  copySync(downloadDirName, realRuntimeDirName)
 
-  readdirSync('deploy-bot/dist').forEach((file) => {
-    const sourceFile = join('deploy-bot/dist', file)
-    const targetFile = join('deploy-bot', file)
+  readdirSync(`${realRuntimeDirName}/dist`).forEach((file) => {
+    const sourceFile = join(`${realRuntimeDirName}/dist`, file)
+    const targetFile = join(`${realRuntimeDirName}`, file)
     moveSync(sourceFile, targetFile, { overwrite: true })
   })
 
-  const packages = readFileSync('deploy-bot/package.json', 'utf8')
+  const packages = readFileSync(`${realRuntimeDirName}/package.json`, 'utf8')
   const injectedPackages = packages
     .replaceAll('workspace:^', '0.0.1')
     .replaceAll('"type": "module"', '"type": "commonjs"')
-  writeFileSync('deploy-bot/package.json', injectedPackages)
+  writeFileSync(`${realRuntimeDirName}/package.json`, injectedPackages)
 
-  process.chdir('deploy-bot')
+  process.chdir(`${realRuntimeDirName}`)
   execSync('pm2 start ecosystem.config.cjs node --', { stdio: 'inherit' })
+
+  readdirSync(downloadDirUri).forEach((file) => {
+    if (file === 'safe-pm2-control.cjs') return
+    manualSync(resolve(downloadDirUri, file))
+  })
 } catch (error) {
   console.error('发生错误:', error)
 }
