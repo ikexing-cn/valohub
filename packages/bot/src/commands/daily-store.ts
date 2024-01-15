3af4d93b323dc3770e431df64d49beaeaf475d2c
ikexing-cn <ikexing@qq.com>
2024-01-15 14:27:24 +0800

    chore: pure esm

--- packages/bot/src/commands/daily-store.ts
+++ packages/bot/src/commands/daily-store.ts
@@ -29,9 +29,7 @@ export async function dailyStoreCommand(
     year: 'numeric',
   })
 
-  const dirname = process.env.DEV ? import.meta.dirname : __dirname
-
-  const dir = resolve(dirname!, `../../screenshots/${qq}/${date}`)
+  const dir = resolve(import.meta.dirname!, `../../screenshots/${qq}/${date}`)
   if (!existsSync(dir)) mkdirSync(dir, { recursive: true })
 
   const imageStorePath = resolve(dir, `daily-store-${alias}.png`)
